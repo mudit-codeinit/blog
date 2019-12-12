0113c2b1e201775d401123111d81981c2606b1a8
prithvi <prithvi@buzznerd.com>
2019-12-12 13:07:59 +0530

    g

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -20,7 +20,7 @@
      <div class="collapse navbar-collapse" id="collapsibleNavbar">
      <ul class="navbar-nav">
        <li class="nav-item">
-       <a class="nav-link active" href="#">Homess</a>
+       <a class="nav-link active" href="#">Homessccccc</a>
        </li>
        <li class="nav-item">
        <a class="nav-link" href="#">Who Is Elissa? </a>
