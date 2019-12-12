ee1662b8b39776ceb0becc3a64acf1777f35aa48
prithvi <prithvi@buzznerd.com>
2019-12-12 14:32:17 +0530

    pifr

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -20,7 +20,7 @@
      <div class="collapse navbar-collapse" id="collapsibleNavbar">
      <ul class="navbar-nav">
        <li class="nav-item">
-       <a class="nav-link active" href="#">HomesssMM</a>
+       <a class="nav-link active" href="#">Home</a>
        </li>
        <li class="nav-item">
        <a class="nav-link" href="#">Who Is Elissa? </a>
