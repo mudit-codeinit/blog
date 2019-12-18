e0c6d96dc36117c4569f7e0ca4e5e902e8ff01db
mudit <mudit.codeinit@gmail.com>
2019-12-18 14:57:47 +0530

    Add HTML FIles

---
+++ client/src/app/login/login.component.ts
@@ -0,0 +1,30 @@
+import {Injectable} from '@angular/core'
+import {Router} from '@angular/router'
+import {AuthenticationService , TokenPayload} from './authentication-service'
+@Component({
+
+  templateUrl: './login.component.html'
+
+})
+export class LoginComponent {
+
+  credentials: TokenPayload = {
+      id: 0,
+      name: "",
+      email: "",
+      password: ""
+  }
+
+constructor(private auth: AuthenticationService, private router: Router){}
+
+  login(){
+        this.auth.login(this.credentials).subscribe(
+          ()=> {
+            this.router.navigateByUrl('/profile')
+          },
+          err => {
+            console.error(err)
+          }
+        )
+  }
+}
---
+++ client/src/app/register/register.component.ts
@@ -0,0 +1,30 @@
+import {Injectable} from '@angular/core'
+import {Router} from '@angular/router'
+import {AuthenticationService , TokenPayload} from './authentication-service'
+@Component({
+
+  templateUrl: './register.component.html'
+
+})
+export class RegisterComponent {
+
+  credentials: TokenPayload = {
+      id: 0,
+      name: "",
+      email: "",
+      password: ""
+  }
+
+constructor(private auth: AuthenticationService, private router: Router){}
+
+  login(){
+        this.auth.register(this.credentials).subscribe(
+          ()=> {
+            this.router.navigateByUrl('/login')
+          },
+          err => {
+            console.error(err)
+          }
+        )
+  }
+}
