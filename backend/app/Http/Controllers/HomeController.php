44522c7a11242ebc49619ddd5f56ccaf9f60e4c6
prithvi <prithvi@buzznerd.com>
2020-01-07 18:20:50 +0530

    fix path

--- backend/app/Http/Controllers/HomeController.php
+++ backend/app/Http/Controllers/HomeController.php
@@ -1,18 +1,18 @@
 <?php
 
 namespace App\Http\Controllers;
- 
+
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
 
     public function index()
-    { 
-
-
-        
+    {
         return view('sitepages.index' );
     }
-    
+    public function about()
+    {
+        return view('sitepages.about' );
+    }
 }
