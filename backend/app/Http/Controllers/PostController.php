c60dddb523cb8e85752bba4a59fe155208571d3a
prithvi <prithvi@buzznerd.com>
2020-01-10 17:37:12 +0530

    Add Auth in backend

--- backend/app/Http/Controllers/PostController.php
+++ backend/app/Http/Controllers/PostController.php
@@ -22,7 +22,22 @@ class PostController extends Controller
 
    public function index()
    {
-       $posts = DB::table('posts')->get();
-       return response()->json(compact('posts'));
+
+       try {
+           if (! $user = JWTAuth::parseToken()->authenticate()) {
+               return response()->json(['user_not_found'], 404);
+           }else{
+              $posts = DB::table('posts')->get();
+              return response()->json(compact('posts'));
+           }
+       } catch (Tymon\JWTAuth\Exceptions\TokenExpiredException $e) {
+           return response()->json(['token_expired'], $e->getStatusCode());
+       } catch (Tymon\JWTAuth\Exceptions\TokenInvalidException $e) {
+           return response()->json(['token_invalid'], $e->getStatusCode());
+       } catch (Tymon\JWTAuth\Exceptions\JWTException $e) {
+           return response()->json(['token_absent'], $e->getStatusCode());
+       }
+
+
    }
 }
